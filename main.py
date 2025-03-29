4c35d0a959795b045e60a700c92827052f2d2909
shamsoddin-mujawar <85243080+shamsoddin-mujawar@users.noreply.github.com>
2025-03-29 11:07:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
-options = Options()
-# REMOVE or COMMENT OUT this line:
-# options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-software-rasterizer")
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
+# Open Google
 driver.get("https://www.google.com")
 
+print("Page Title:", driver.title)
+
+# Close the browser
 driver.quit()
